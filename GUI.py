b22e785c8c53cdbb4e3d7da9d6822d689dd142e6
AnhQuoc <phamanhquoc2014@gmail.com>
2018-10-28 05:37:09 +0700

    Thêm phần chọn file, cần try catch các lỗi: chọn file không đúng định dạng, không đúng cấu trúc, lỗi Destroy đột ngột

--- GUI.py
+++ GUI.py
@@ -1,12 +1,49 @@
+import os
 import heapq as heap
 import math
-from tkinter import Tk, CENTER
-from tkinter.ttk import Label, Frame
+from tkinter import Tk, CENTER, filedialog, BOTH
+from tkinter.ttk import Label, Frame, Button
 import tkinter.font as tkFont
 
 UPDATE_CLOCK = 700
 # Traversaling order to adjacent get_cell, clockwise
 ADJ_ORDER = [(-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1)]
+COLOR_GOAL = "blue"
+COLOR_START = "red"
+COLOR_CURRENT_POINT = "white"
+COLOR_VISITED_POINT = "orange"
+COLOR_HEAP = "green"
+COLOR_ROUTE = "blue"
+COLOR_BLOCK = "black"
+
+
+class OpenFileWindow(Frame):
+    def __init__(self, parent):
+        Frame.__init__(self, parent)
+
+        self.parent = parent
+        self.initUI()
+
+    def initUI(self):
+        self.parent.title("Open File Window")
+        self.pack(fill=BOTH, expand=1)
+
+        btn_choose_file = Button(self, text='Open File')
+        btn_choose_file.place(x=60, y=35)
+        btn_choose_file['command'] = self.choose_file
+
+    def choose_file(self):
+        curr_directory = os.getcwd()
+        filename = filedialog.askopenfilename(initialdir=curr_directory,
+                                              title="Select file",
+                                              filetypes=(
+                                                  ("Text files", "*.txt"),
+                                                  ("All files", "*.*")
+                                              )
+                                              )
+        if filename:
+            gui = App(filename)
+            gui.run()
 
 
 class Point:
@@ -34,7 +71,7 @@ class App:
     - Play button to Start process A* search
     """
 
-    def __init__(self):
+    def __init__(self, filename):
 
         self.queue = []
         self.distance = -1
@@ -54,25 +91,17 @@ class App:
         self.lbl_fn = Label(self.form, text="f(n) = 0.0", justify=CENTER)
         self.lbl_gn = Label(self.form, text="g(n) = 0.0", justify=CENTER)
         self.lbl_hn = Label(self.form, text="h(n) = 0.0", justify=CENTER)
-        self.lbl_distance = Label(self.form, text="Distance: ", justify=CENTER)
+        self.lbl_distance = Label(self.form, text="Final Distance: ", justify=CENTER)
 
-        self.color_goal = "blue"
-        self.color_start = "red"
-        self.color_current_point = "white"
-        self.color_visited_point = "orange"
-        self.color_heap = "green"
-        self.color_route = "blue"
-        self.color_block = "black"
-
-        self.input()
+        self.input(filename)
         heap.heappush(self.queue, (0, self.start))
         self.init_ui()
         self.updater()
 
     # Read input
     # Remember to replace input.txt to sys.argv[1]
-    def input(self):
-        with open('input.txt') as f:
+    def input(self, filename):
+        with open(filename) as f:
             self.size = int(next(f))
             self.start = tuple([int(x) for x in next(f).split()])
             self.goal = tuple([int(x) for x in next(f).split()])
@@ -109,14 +138,14 @@ class App:
                 if self.matrix[i][j].valid:
                     self.labels[i].append(self.create_cell(i, j))
                 else:
-                    self.labels[i].append(self.create_cell(i, j, self.color_block))
+                    self.labels[i].append(self.create_cell(i, j, COLOR_BLOCK))
 
-        self.get_label(self.start).configure(background=self.color_start, text="S")
-        self.get_label(self.goal).configure(background=self.color_goal, text="G")
+        self.get_label(self.start).configure(background=COLOR_START, text="S")
+        self.get_label(self.goal).configure(background=COLOR_GOAL, text="G")
 
-        self.lbl_fn.grid(row=self.size, columnspan=self.size)
-        self.lbl_gn.grid(row=self.size + 1, columnspan=self.size)
-        self.lbl_hn.grid(row=self.size + 2, columnspan=self.size)
+        # self.lbl_fn.grid(row=self.size, columnspan=self.size)
+        # self.lbl_gn.grid(row=self.size + 1, columnspan=self.size)
+        # self.lbl_hn.grid(row=self.size + 2, columnspan=self.size)
         self.lbl_distance.grid(row=self.size + 3, columnspan=self.size)
 
         for i in range(self.size):
@@ -162,21 +191,22 @@ class App:
                 while p != self.start:
                     self.queue = []
                     self.path = [p] + self.path
-                    self.get_label(p).configure(background=self.color_route)
+                    self.get_label(p).configure(background=COLOR_ROUTE)
                     p = self.get_cell(p).trace
 
                 self.path = [self.start] + self.path
+                self.lbl_distance.configure(text="Final Distance: " + str(self.distance))
 
             else:
                 if p != self.start:
-                    self.get_label(p).configure(background=self.color_visited_point)
+                    self.get_label(p).configure(background=COLOR_VISITED_POINT)
 
                 for adj in ADJ_ORDER:
                     padj = (p[0] + adj[0], p[1] + adj[1])
                     if self.valid_pos(padj) and self.f_astar(padj) > self.f_astar(p) + 1:
                         self.get_cell(padj).dist = self.get_cell(p).dist + 1
                         self.get_cell(padj).trace = p
-                        self.get_label(padj).configure(background=self.color_heap)
+                        self.get_label(padj).configure(background=COLOR_HEAP)
                         heap.heappush(self.queue, (self.get_cell(padj).dist, padj))
 
         # self.form.after(UPDATE_CLOCK, self.next_step())
@@ -186,5 +216,8 @@ class App:
         self.form.after(UPDATE_CLOCK, self.updater)
 
 
-app = App()
-app.run()
+root = Tk()
+root.geometry("200x100")
+app = OpenFileWindow(root)
+root.mainloop()
+
